c79cf1f4ed3e102edc8eacf8272c1fe1a7ee0845
RaulLlanos <106988197+RaulLlanos@users.noreply.github.com>
2022-06-22 12:25:25 -0400

    Add files via upload

---
+++ avion.py
@@ -0,0 +1,30 @@
+import numpy as np
+#arange =Rellena la lista con un rango inicio,fin
+#reshape = organiza el arreglo nuevamente por filas y columna
+asientos=np.arange(1,43).reshape((7,6))
+
+opt=0
+while opt != 5:
+    try:
+        
+        print("Menu: ")
+        print("1.- Ver asientos disponibles \n2.- Comprar asiento \n3.- Anular vuelo \n4.- Modificar datos de pasajero \n5.- Salir")
+
+        opt=int(input("Seleccione la opcion que desea: "))
+
+        if opt == 1:
+
+            print(f"los asientos disponibles son: \n{asientos}")
+        
+        if opt == 2:
+            print("El valor de los asientos son los siguintes\n")
+            print("1.- Normal: $78.900 \n2.- Premium: $240.000")
+
+        
+
+
+    
+    except ValueError:
+        print("El valor debe ser entre 1 a 4 ")
+    if opt > 4:
+        raise Exception("Opcion debe estar entre 1 a 4")
